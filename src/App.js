84aed6162d8fb2942380693c79774aae74c7b77f
Janaina Franquis <39539970+jfranquis@users.noreply.github.com>
2021-05-26 14:10:37 -0300

    atualização

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { AppContainer } from './components/AppContainer'
-import Carrinho from './components/Carrinho'
+import React from 'react';
+import { AppContainer } from './components/AppContainer';
+import Carrinho from './components/Carrinho';
+import axios from 'axios';
 
 
 function App() {
