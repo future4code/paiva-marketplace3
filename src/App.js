c3f21b23c27fd17b2eded2d4bc89075a0d5ee9df
Thiago Felipe <thiago.felipe@tutanota.com>
2021-05-26 13:58:59 -0400

    Fazendo correções de bug que foram encontrados no merge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Inicial from "./Páginas/Inicial";
 import Prestador from "./Páginas/Prestador";
 import Contratante from "./Páginas/Contratante";
-=======
-import React from 'react'
-import { AppContainer } from './components/AppContainer'
-import { Body } from './components/Body/Body'
->>>>>>> 26e0316646d76164ec798c7faf27494d33c4b27d
 
 class App extends Component {
   state = { página: "Início" }
 
-<<<<<<< HEAD
   escolherPágina = () => {
     if (this.state.página === "Início")
       return (
@@ -48,49 +41,13 @@ class App extends Component {
     const página = this.escolherPágina();
 
     return (
-      <>
+      <Fragment>
         <Header/>
         {página}
         <Footer/>
-      </>
+      </Fragment>
     );
   }
 }
 
 export default App;
-=======
-export default class App extends React.Component{
-	state = {
-		pagina: ""
-	}
-
-	proximaPagina = () => {
-		if (this.state.pagina ===  ""){
-			this.setState({pagina: ""})
-		} else if (this.state.pagina === ""){
-			this.setState({pagina:""})
-		}
-	}
-
-/*	renderizandoPagina = () => {
-		switch(this.state.pagina){
-			case "":
-				return <""/>
-			case "":
-				return <""/>
-			default:
-
-		}
-	}	*/
-	render () {
-	  return (
-        <div>
-          <AppContainer />
-          <br/>
-        </div>
-	  )
-  }
-}
-
-
->>>>>>> 26e0316646d76164ec798c7faf27494d33c4b27d
--- src/Páginas/Inicial.js
+++ src/Páginas/Inicial.js
@@ -1,15 +1,71 @@
-import React from "react";
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+// import styled from "styled-components";
+import foto from "../img/peoples.png";
 
-export default function Inicial(props) {
+/*const section = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 50px;
+  justify-items: center;
+  align-items: center;
+`;*/
+
+const Body = () => {
   return (
-    <main>
-      <button onClick={props.irParaPrestador}>
-        Ir Para A Página De Prestador
-      </button>
-      <button onClick={props.irParaContratante}>
-        Ir Para A Página De Contratante
-      </button>
-      <p>Página Inicial</p>
-    </main>
+    <div>
+      <img src={foto} alt="people"/>
+    </div>
   );
+};
+
+
+export default class Inicial extends Component {
+  render() {
+    return (
+      <div>
+        <section className="CardQuemSomos">
+          <h2>Quem somos</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />
+            Ad veniam laboriosam maiores est excepturi laudantium voluptates?
+            Ipsa minus impedit vero!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br />
+            Ad veniam laboriosam maiores est excepturi laudantium voluptates?
+            Ipsa minus impedit vero!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+            fugiat, <br />
+            placeat explicabo ab eum sequi vel ipsum, aspernatur voluptatibus
+            magnam unde <br />
+            dolores recusandae vero eveniet cupiditate, itaque repellendus iste.
+            Magni?
+          </p>
+        </section>
+
+        <section className="botaoBody">
+          <Button 
+            variant="contained" 
+            color="primary"
+            onClick={this.props.irParaPrestador}
+          >
+            Prestador de serviços
+          </Button>
+          <br />
+          <Button 
+            variant="contained" 
+            color="primary"
+            onClick={this.props.irParaContratante}
+          >
+            Contratante
+          </Button>
+        </section>
+
+        <section></section>
+      </div>
+    );
+  }
 }
