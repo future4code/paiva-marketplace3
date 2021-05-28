7aded335fc5f4a3980df84cab1b8d5d07ee9bc56
júllia Izidorio <contato.julliaizidorio@gmail.com>
2021-05-28 18:09:23 -0300

    estilização do prestador pronta

--- src/components/FormPrestador.js
+++ src/components/FormPrestador.js
@@ -1,16 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import axios from "axios";
+import Button from "@material-ui/core/Button";
 
 const Section = styled.section`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  padding: 100px;
+  padding: 2%;
   top: 15px;
+  gap:1%;
   justify-items: center;
   align-items: center;
 `;
-
 const CardCadastro = styled.div`
   border: 1px solid black;
   display: flex;
@@ -18,16 +19,32 @@ const CardCadastro = styled.div`
   justify-content: space-around;
   row-gap: 10px;
   padding: 40px;
-  height: 350px;
+  height: 50vh;;
+  width:50vw;
+  justify-content:space-around;
+  text-align:center;
 `;
-
 const CardServiços = styled.div`
-  border: 1px solid black;
   display: flex;
   flex-direction: column;
   padding: 40px;
 `;
+const Servico = styled.div`
+display:flex;
+text-align:center;
+justify-content:space-between;
+border-radius:18px;
+border-bottom:2px solid;
+padding:2%;
+margin:2%;
+width:30vw;
+`
+const Separar =  styled.div`
+display:flex;
+justify-content:space-around;
+`
 //----------------API-------------//
+
 const url = `https://labeninjas.herokuapp.com/jobs`;
 const header = {
     headers: {
@@ -37,15 +54,15 @@ const header = {
 //----------------API-------------//
 
 export default class FormPrestador extends React.Component {
-    state ={
-        title: "", 
-        description: "", 
-        price: "", 
-        paymentMethods: "", 
-        dueDate: "", 
-        serviços:[]
+    state = {
+        title: "",
+        description: "",
+        price: "",
+        paymentMethods: "",
+        dueDate: "",
+        serviços: []
     }
-        
+
     changeInputValues = (e) => {
         if (e.target.id === "pagamento") {
             this.setState({ [e.target.name]: [e.target.value] });
@@ -59,7 +76,7 @@ export default class FormPrestador extends React.Component {
         await axios
             .get(url, header)
             .then((certo) => {
-                this.setState({serviços:certo.data.jobs})
+                this.setState({ serviços: certo.data.jobs })
                 console.log(certo.data.jobs)
             })
             .catch((errado) => {
@@ -93,16 +110,17 @@ export default class FormPrestador extends React.Component {
                 alert(err.response.data.message);
             });
     };
-    
+
 
     render() {
         console.log(this.state)
-        const mostraServiços = this.state.serviços.map((serviço)=>{
-            return <CardServiços>{serviço.title}
-               <button>X</button>
-            </CardServiços>
+        const mostraServiços = this.state.serviços.map((serviço) => {
+            return <Servico>{serviço.title}
+                <Button variant="contained"
+                        color="primary">X</Button>
+            </Servico>
         })
-                 
+
         return (
             <Section>
                 <CardCadastro>
@@ -127,7 +145,7 @@ export default class FormPrestador extends React.Component {
                         value={this.state.price}
                         onChange={this.changeInputValues}
                     />
-                    <div>
+                    <Separar>
                         <select
                             placeholder="Forma de pagamento"
                             name="paymentMethods"
@@ -147,16 +165,18 @@ export default class FormPrestador extends React.Component {
                             value={this.state.dueDate}
                             onChange={this.changeInputValues}
                         />
-                    </div>
-                    <button onClick={this.createJob}>Cadastrar serviços</button>
+                    </Separar>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={this.createJob}>Cadastrar serviços</Button>
                 </CardCadastro>
                 <div>
                     <CardServiços>
-                        <h2>SERVIÇOS CADASTRADOS</h2>                       
+                        <h2>SERVIÇOS CADASTRADOS</h2>
 
                         <p>{mostraServiços}</p>
-                     
-                        
+
                         {/* retorna serviços cadastrados com botão de delete */}
                     </CardServiços>
                 </div>
